e3525a6d95e5964c7168aaac300ab52f0ec3c98b
Akaeid flutter <akaeidcse772440@gmail.com>
2026-07-31 09:51:37 +0600

    Add files via upload
    This project just add lib folder because not supported 100 or higher folder.

---
+++ lib/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:taskmanager/ui/Controller/Login_Controller.dart';
+import 'package:taskmanager/ui/Controller/New_task_Controller.dart';
+import 'package:taskmanager/ui/Screens/SplashScreen.dart';
+import 'package:taskmanager/ui/Screens/loginScreen.dart';
+
+class TaskManager extends StatefulWidget {
+  const TaskManager({Key? key}) : super(key: key);
+
+  static GlobalKey<NavigatorState> globalKey = GlobalKey<NavigatorState>();
+
+  @override
+  State<TaskManager> createState() => _TaskManagerState();
+}
+
+class _TaskManagerState extends State<TaskManager> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      navigatorKey: TaskManager.globalKey,
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      routes: {
+        '/LoginScreen': (context) => const LoginScreen(),
+      },
+      getPages: [
+        GetPage(name: '/LoginScreen', page: () => const LoginScreen()),
+      ],
+      initialBinding: ControllerBinder(),
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        primarySwatch: Colors.green
+      ),
+    );
+  }
+}
+class ControllerBinder extends Bindings{
+  @override
+  void dependencies() {
+    Get.put(LoginController());
+    Get.put(NewTaskController());
+  }
+
+}
